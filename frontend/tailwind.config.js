009723a4947519c8b575058353011911a3e5d8d0
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-09-25 16:19:51 -0400

    add daisyui

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+
+    themes: [
+      {
+        mytheme: {
+          primary: "#3ABFF8",
+
+          secondary: "#828DF8",
+
+          accent: "#F471B5",
+
+          neutral: "#1D283A",
+
+          "base-100": "#0F1729",
+
+          info: "#0CA6E9",
+
+          success: "#2BD4BD",
+
+          warning: "#F4C152",
+
+          error: "#FB6F84",
+        },
+      },
+    ],
+  },
+};
