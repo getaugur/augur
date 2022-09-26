b8f2c8c0ecd09af35184ab93acbc748b44428828
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-09-25 20:46:32 -0400

    process data in central place instead of gorse
    Started to push documents to meili as well as disabled gorse due to axios-logger requirement

--- frontend/src/utils/gorse.ts
+++ frontend/src/utils/gorse.ts
@@ -1,7 +1,5 @@
 import { Gorse, Item } from "gorsejs";
-import { Media } from "@prisma/client";
-import { prisma } from "../server/db/client";
-import { MediaWithIDs, UpdatedMedia } from "./processDocuments";
+import { UpdatedMediaData } from "./processDocuments";
 
 export const gorseClient = new Gorse({
   endpoint: process.env.GORSE_ENDPOINT || "",
@@ -16,7 +14,7 @@ export async function updateGorseUser(userId: string, provider: string) {
   });
 }
 
-export async function updateGorseMedia(updatedMedia: MediaWithIDs[]) {
+export async function updateGorseMedia(updatedMedia: UpdatedMediaData[]) {
   const items: Item[] = [];
 
   for (let i = 0; i < updatedMedia.length; i++) {
@@ -24,49 +22,17 @@ export async function updateGorseMedia(updatedMedia: MediaWithIDs[]) {
 
     if (media === undefined) continue;
 
-    const labels = extractLabels(media);
-
     items.push({
-      ItemId: `${media.title}-${media.year}`,
+      ItemId: media.id,
       Comment: media.title,
       IsHidden: false,
-      Timestamp: new Date(media.firstAired || media.released || ""),
-      Categories: [media.mediaIds.mediaType, ...media.genres],
-      Labels: [...media.genres, ...labels, ...media.people],
+      Timestamp: media.timestamp,
+      Categories: media.categories,
+      Labels: media.labels,
     });
   }
 
   gorseClient.upsertItems(items);
 
   console.log(`Added ${items.length} items to gorse`);
-}
-
-export function extractLabels(media: Media): string[] {
-  const labels: string[] = [];
-
-  // media.availableTranslations.forEach((item) => {
-  //   labels.push(`Translation: ${item}`);
-  // });
-
-  if (media.certification !== null)
-    labels.push(`Certification: ${media.certification}`);
-
-  if (media.country !== null) labels.push(`Country: ${media.country}`);
-
-  labels.push(`Language: ${media.language}`);
-
-  if (media.network !== null) labels.push(`Network: ${media.network}`);
-
-  labels.push(`Rating: ${media.rating.toFixed(2)}`);
-
-  labels.push(`Status: ${media.status}`);
-
-  if (media.trailer !== null) labels.push("Trailer");
-
-  labels.push(`Year: ${media.year}`);
-
-  if (media.firstAired !== null || media.released !== null)
-    labels.push("Released");
-
-  return labels;
 }
--- frontend/src/utils/processDocuments.ts
+++ frontend/src/utils/processDocuments.ts
@@ -1,10 +1,15 @@
 import { Media, MediaIds } from "@prisma/client";
 import { prisma } from "../server/db/client";
 import { updateGorseMedia } from "./gorse";
+import { updateMeiliMedia } from "./meili";
 
-export type MediaWithIDs = Media & {
-  mediaIds: MediaIds;
-};
+export interface UpdatedMediaData {
+  id: string;
+  title: string;
+  timestamp: Date;
+  categories: string[];
+  labels: string[];
+}
 
 export interface UpdatedMedia {
   title: string;
@@ -14,7 +19,7 @@ export interface UpdatedMedia {
 export async function processDocuments(
   updatedMedia: UpdatedMedia[]
 ): Promise<void> {
-  const items: MediaWithIDs[] = [];
+  const items: UpdatedMediaData[] = [];
 
   for (let i = 0; i < updatedMedia.length; i++) {
     const mediaObj = updatedMedia[i];
@@ -33,8 +38,47 @@ export async function processDocuments(
       },
     });
 
-    if (media !== undefined && media !== null) items.push(media);
+    if (media === undefined || media === null) continue;
+
+    items.push({
+      id: media.mediaIds.traktSlug,
+      title: media.title,
+      timestamp: new Date(media.firstAired || media.released || ""),
+      categories: [media.mediaIds.mediaType, ...media.genres],
+      labels: [...media.genres, ...extractLabels(media), ...media.people],
+    });
   }
 
-  updateGorseMedia(items);
+  //   updateGorseMedia(items);
+  updateMeiliMedia(items);
+}
+
+export function extractLabels(media: Media): string[] {
+  const labels: string[] = [];
+
+  // media.availableTranslations.forEach((item) => {
+  //   labels.push(`Translation: ${item}`);
+  // });
+
+  if (media.certification !== null)
+    labels.push(`Certification: ${media.certification}`);
+
+  if (media.country !== null) labels.push(`Country: ${media.country}`);
+
+  labels.push(`Language: ${media.language}`);
+
+  if (media.network !== null) labels.push(`Network: ${media.network}`);
+
+  labels.push(`Rating: ${media.rating.toFixed(2)}`);
+
+  labels.push(`Status: ${media.status}`);
+
+  if (media.trailer !== null) labels.push("Trailer");
+
+  labels.push(`Year: ${media.year}`);
+
+  if (media.firstAired !== null || media.released !== null)
+    labels.push("Released");
+
+  return labels;
 }
