74af70d33d7530224cd5bd3e6095896a953902d1
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-09-26 20:16:52 -0400

    functional search box

--- frontend/src/components/searchbar.tsx
+++ frontend/src/components/searchbar.tsx
@@ -1,8 +1,9 @@
-import { MeiliSearch, Hits } from "meilisearch";
+import { MeiliSearch, Hits, SearchResponse } from "meilisearch";
 import { useEffect, useMemo, useState } from "react";
+import Link from "next/link";
 
 export default function Component() {
-  const [movies, setMovies] = useState<Hits<Record<string, any>>>([]);
+  const [media, setMedia] = useState<SearchResponse<Record<string, any>>>();
   const [search, setSearch] = useState("");
 
   const client = useMemo(
@@ -14,26 +15,41 @@ export default function Component() {
     []
   );
 
-  //   useEffect(() => {
-  //     //search movie index based on search value
-  //     client
-  //       .index("media")
-  //       .search(search, {
-  //         limit: 5,
-  //       })
-  //       .then((results) => {
-  //         setMovies(results.hits);
-  //       });
-  //   }, [client, search]);
+  useEffect(() => {
+    //search movie index based on search value
+    client
+      .index("media")
+      .search(search, {
+        limit: 5,
+      })
+      .then((results) => {
+        setMedia(results);
+      });
+  }, [client, search]);
 
   return (
-    <div className="form-control">
-      <input
-        type="text"
-        placeholder="Search"
-        className="input input-bordered"
-        onChange={(e) => setSearch(e.target.value)}
-      />
+    <div className="dropdown dropdown-end">
+      <div className="form-control">
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4"
+      >
+        {search.length > 0 &&
+          media?.hits.map((value) => (
+            <li key={value.id}>
+              <Link href={`/${value.categories[0].toLowerCase()}/${value.id}`}>
+                <a>{value.title}</a>
+              </Link>
+            </li>
+          ))}
+      </ul>
     </div>
   );
 }
