c725c3de5fd0c82074c8f96be885a48daf935caa
Husky <39809509+Huskydog9988@users.noreply.github.com>
2022-10-08 11:00:40 -0400

    make augur in nav a link

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,11 +1,14 @@
 import ProfileLogin from "./profile-login";
 import SearchBar from "./searchbar";
+import Link from "next/link";
 
 export default function Component() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">Augur</a>
+        <Link href={"/"}>
+          <a className="btn btn-ghost normal-case text-xl">Augur</a>
+        </Link>
       </div>
       <div className="flex-none">
         <SearchBar />
